624fcb0feaeacf8e0b0f05e5e94f244200d1f775
gaellerem <146330927+gaellerem@users.noreply.github.com>
2025-01-20 14:15:53 +0100

    update colors

--- file.py
+++ file.py
@@ -14,21 +14,21 @@ def get_color(temp):
     elif 12 <= temp < 15:
         return '#D5C7E9'
     elif 15 <= temp < 18:
-        return '#F6CFDF'
+        return '#F0B2CB'
     elif 18 <= temp < 21:
-        return '#B62C81'
+        return '#F785C9'
     elif 21 <= temp < 24:
-        return '#AE2958'
+        return '#B62C81'
     elif 24 <= temp < 27:
-        return '#F36973'
+        return '#AE2958'
     elif 27 <= temp < 30:
-        return '#F74E3C'
-    elif 30 <= temp < 33:
-        return '#E80128'
-    elif 33 <= temp < 36:
-        return '#811530'
-    elif 36 <= temp :
         return '#632243'
+    elif 30 <= temp < 33:
+        return '#811530'
+    elif 33 <= temp < 36:
+        return '#E80128'
+    elif 36 <= temp :
+        return '#F74E3C'
 
 def extract_data(filepath):
     df = pd.read_csv(filepath, sep=';', names=['Date', 'TempMin', 'TempMax'])
