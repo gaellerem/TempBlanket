24002476233c42599b6ff11e5cb4ae141cee77df
gaellerem <146330927+gaellerem@users.noreply.github.com>
2025-02-09 12:35:19 +0100

    creating ui file with a template

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from draw import draw_hexagons
 from file import extract_data
 from pairs import get_color_pairs
+from ui_template.template import make_ui
 
 df = extract_data('data.csv')
 # draw_hexagons(22, 18, 1, df)
 # get_color_pairs(df)
+
+make_ui()
---
+++ ui_template/template.py
@@ -0,0 +1,77 @@
+from string import Template
+
+def make_ui():
+    with open('ui_template/template.txt', 'r') as f:
+        template = Template(f.read())
+
+    spinboxes = ''
+    with open('color_pairs.csv', 'r') as f:
+        for i, line in enumerate(f):
+            if i == 0: continue
+            line = line.strip().split(';')
+            spinboxes += f"""
+            <item>
+            <widget class="QWidget" name="widget_{i}" native="true">
+                <layout class="QHBoxLayout" name="horizontalLayout_{i}">
+                    <property name="leftMargin">
+                        <number>0</number>
+                    </property>
+                    <property name="topMargin">
+                        <number>0</number>
+                    </property>
+                    <property name="rightMargin">
+                        <number>0</number>
+                    </property>
+                    <property name="bottomMargin">
+                        <number>0</number>
+                    </property>
+                <item>
+                    <widget class="QLabel" name="label_{i}">
+                        <property name="maximumSize">
+                            <size>
+                                <width>75</width>
+                                <height>75</height>
+                            </size>
+                        </property>
+                        <property name="toolTip">
+                            <string>{line[1]}_{line[2]}</string>
+                        </property>
+                        <property name="text">
+                            <string/>
+                        </property>
+                        <property name="pixmap">
+                            <pixmap resource="resource.qrc">:/pairs/pairs/{line[1]}_{line[2]}.png</pixmap>
+                        </property>
+                        <property name="scaledContents">
+                            <bool>true</bool>
+                        </property>
+                    </widget>
+                </item>
+                <item>
+                    <widget class="QSpinBox" name="spinBox_{i}">
+                    <property name="maximumSize">
+                    <size>
+                    <width>50</width>
+                    <height>16777215</height>
+                    </size>
+                    </property>
+                    <property name="minimum">
+                    <number>0</number>
+                    </property>
+                    <property name="maximum">
+                    <number>{int(line[0])}</number>
+                    </property>
+                    </widget>
+                </item>
+                <item>
+                    <widget class="QLabel" name="label_{i}_2">
+                        <property name="text">
+                        <string>/{line[0]}</string>
+                        </property>
+                    </widget>
+                </item>
+            </layout>
+            </widget>
+            </item>"""
+    with open('main.ui', 'w', encoding="utf-8") as f:
+        f.write(template.substitute(spinboxes=spinboxes))
